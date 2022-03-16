bbd577a09a6093d8f3253e7ba012327e1339a440
justice <justin.etchell@gmail.com>
2022-03-16 12:38:38 +1100

    Typos

--- StringTheory/Program.cs
+++ StringTheory/Program.cs
@@ -13,17 +13,19 @@ namespace StringTheory
             Sequence test = new Sequence();
             test.AddEvent(new DynamicString(". . . ", clearScreen: true, delay: 500, delayPerChar: true));
             test.AddEvent(new DynamicString("Welcome . . ", delay: 45, delayPerChar: true));
-            test.AddEvent(new DynamicString(delay: 500));
+            test.AddEvent(new Pause(500));
 
             Sequence test2 = new Sequence();
             test2.AddEvent(new DynamicString("We've been waiting for you", clearScreen: true, delay: 250));
-            test2.AddEvent(new DynamicString(delay: 500, newLine: false));
+            test2.AddEvent(new Pause(500));
             test2.AddEvent(new DynamicString("Have cake.", delay: 500, colour: ConsoleColor.DarkRed));
             test2.AddEvent(new Choose(new string[] { "Eat cake", "Don't eat cake" }, ref testInt));
 
             test.AddSequence(test2);
 
             test.StartSequence();
+
+            new DynamicString("I am not part of a sequence!", delay: 55, delayPerChar: true).Run();
         }
     }
 }
